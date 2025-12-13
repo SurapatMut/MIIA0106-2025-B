ebfe264877725d81cee50586992ca1f9bf483bcc
SurapatMut <6812100021@mut.ac.th>
2025-12-13 08:37:45 +0700

    LAB 3

---
+++ Lab3/3.1/3.1/3.1.cpp
@@ -0,0 +1,25 @@
+// Test02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int age = 21;
+    double gpa = 3.25;
+    string name = "Surapat Krutta";
+    string id = "6812100021";
+
+    cout << "Student Profile" << endl;
+    cout << "----------------" << endl;
+    cout << "Name:" << name << endl;
+    cout << "Age:" << age << endl;
+    cout << "Student ID:" << id << endl;
+    cout << "GPA:" << gpa << endl;
+
+    return 0;
+
+
+}
